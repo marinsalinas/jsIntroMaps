3a86b6616a923ee0bd699d4a45fade64f23e44c0
Marín Salinas <marinssalinas@gmail.com>
2015-03-14 07:23:57 -0600

    Inicializando mapa de google

--- gmap.js
+++ gmap.js
@@ -1,1 +1,18 @@
-console.log("esto si se ejecuta");
+
+var map;
+function initialize() {
+  var mapDiv = document.getElementById('map_canvas');
+  if(!mapDiv){
+  	alert("No es posible cargar el mapa ya que no se encuentra su div contenedor");
+  	return
+  }
+  
+   var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(mapDiv,
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
